23f18c63dd480ddc6e386e21d599a499d8609e94
tracugai2810 <tracugaianthai@gmail.com>
2026-08-13 14:04:46 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -702,6 +702,7 @@ function renderCaptureHTML(data) {
                         }
                         return `<div class="ss-item">${s}</div>`;
                     }).join('');
+                })()}
                 <div class="contact-info" style="grid-column: 3 / 5; text-align: center; align-self: end; padding-bottom: 5px; font-size: 17px; color: #1a1a1a; font-weight: 700; font-style: italic;">
                     <span style="color: #e60000; font-family: Arial, sans-serif;">Dịch Hoàng</span> <span style="font-family: Arial, sans-serif;">0327308506 (ZALO)</span>
                 </div>
